179cdf1480d06683a1f10893b69e2025a392b613
Neisser Sánchez <rko_619_fbi@hotmail.com>
2020-08-17 11:11:58 -0500

    conexión con servidor

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ Class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=reservas-hotel",
-						"root",
-						"");
+		$link = new PDO("mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_c52fdbbfef36bbd",
+						"bed3337fb04d4b",
+						"0442fb7e");
 
 		$link->exec("set names utf8");
 
